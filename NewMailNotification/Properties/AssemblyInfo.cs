7652c389f1b0193c3aaa0d80d4f2927d588aeed6
Yves Goergen <yves@unclassified.de>
2016-03-07 23:23:05 +0100

    Version 1.1

--- NewMailNotification/Properties/AssemblyInfo.cs
+++ NewMailNotification/Properties/AssemblyInfo.cs
@@ -8,14 +8,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("unclassified software development")]
 
 // Assembly identity version. Must be a dotted-numeric version.
-[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyVersion("1.1")]
 
 // Repeat for Win32 file version resource because the assembly version is expanded to 4 parts.
-[assembly: AssemblyFileVersion("1.0")]
-
-// Informational version string, used for the About dialog, error reports and the setup script.
-// Can be any freely formatted string containing punctuation, letters and revision codes.
-[assembly: AssemblyInformationalVersion("1.0")]
+[assembly: AssemblyFileVersion("1.1")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
